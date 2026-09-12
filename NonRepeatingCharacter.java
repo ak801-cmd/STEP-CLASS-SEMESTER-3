ea1bcee378822e16c1934fec0914477aa89185de
ak801-cmd <arpitkyadav66@gmail.com>
2026-09-12 16:23:50 +0530

    Add files via upload

---
+++ NonRepeatingCharacter.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class NonRepeatingCharacter {
+
+    static char findFirstNonRepeatingChar(String text) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char ch = text.charAt(i);
+
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char ch = text.charAt(i);
+
+            if (map.get(ch) == 1)
+                return ch;
+        }
+
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        char result = findFirstNonRepeatingChar(text);
+
+        if (result == '\0')
+            System.out.println("No Non-Repeating Character Found");
+        else
+            System.out.println("First Non-Repeating Character: '" + result + "'");
+
+        sc.close();
+    }
+}
