aed0512c2e22c7d4bd66ed216fb196ba4d44fc75
ak801-cmd <arpitkyadav66@gmail.com>
2026-09-13 10:05:51 +0530

    Add files via upload

---
+++ EmployeeProfile.java
@@ -0,0 +1,43 @@
+class EmployeeData
+{
+    String empId;
+    String empName;
+    double salary;
+    boolean isIntern;
+
+    public EmployeeData(String empId, String empName, double salary)
+    {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.isIntern = false;
+    }
+
+    public EmployeeData(String empId, String empName)
+    {
+        this(empId, empName, 0);
+        this.isIntern = true;
+    }
+
+    public void printProfile()
+    {
+        System.out.println(empId + " | " + empName
+                + " | Rs " + salary
+                + " | Intern: " + isIntern);
+    }
+}
+
+public class EmployeeProfile
+{
+    public static void main(String[] args)
+    {
+        EmployeeData permanent =
+                new EmployeeData("E-101", "Divya", 65000);
+
+        EmployeeData intern =
+                new EmployeeData("E-102", "Arjun");
+
+        permanent.printProfile();
+        intern.printProfile();
+    }
+}
